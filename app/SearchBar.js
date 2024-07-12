e4f4cef1abc6ec2868fd64281f55149926edde50
swari <swari21@navgurukul.org>
2024-07-12 17:56:18 -0400

    displaying information about movies

--- app/SearchBar.js
+++ app/SearchBar.js
@@ -3,27 +3,23 @@ import React, { useState } from 'react';
 import MovieDetails from './MovieDetails'; 
 import './globals.css';  
 
-const SearchBar = ({ onSearch }) => {
+const SearchBar = () => {
   const [movie, setMovie] = useState(null);
   const [search, searchTitle] = useState('');
 
   const searchMovie = () => {
-    console.log('Searching for movie:', search);
+    // console.log('Searching for movie:', search);
     fetch(`http://www.omdbapi.com/?t=${search}&apikey=8bf35543`)
       .then(response => response.json())
       .then(data => {
-        console.log('API response:', data);
+        // console.log('API response:', data);
         setMovie(data);
-        if (onSearch) {
-          onSearch(data);
-        }
       });
   };
 
   return (
     <div className="container">
         <div className="search-box">
-          <img className="img-search" src="Frame.png" alt="Search" />
           <input
             type="text"
             className="form-control"
