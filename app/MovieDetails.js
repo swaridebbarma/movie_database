d8d6d85462e0be4c65dafdcc4176d53db766adc9
swari <swari21@navgurukul.org>
2024-07-13 07:54:49 -0400

    i made some changes to the css

--- app/MovieDetails.js
+++ app/MovieDetails.js
@@ -1,41 +1,26 @@
 import React from 'react';
-import "./MovieDetails.css"
+import './MovieDetails.css';
 
 const MovieDetails = ({ movie }) => {
   if (!movie) return null;
-
   return (
-    <div className='movie-row-main'>
-      <div className='col1'>
-        <img src={movie.Poster} className="card-img-top" alt={movie.Title} /></div>
-      <div className='col2'>
-        <div className='col-1'>
-          <p className="card"><strong>hName:</strong>{movie.Title}</p>
-        </div>
-        <div className='col-2'>
-          <p className="card"><strong>Actors:</strong>{movie.Actors}</p>
-        </div>
-        <div className='col-3'>
-          <p className="card"><strong>Released:</strong> {movie.Released}</p>
-        </div>
-        <div className='col-4'>
-          <p className="card"><strong>Directon:</strong> {movie.Director}</p>
-        </div>
-        <div className='col-5'>
-          <p className="card"><strong>Genre:</strong> {movie.Genre}</p>
-        </div>
-        <div className='col-6'>
-          <p className="card"><strong>Country:</strong> {movie.Country}</p>
-        </div>
-        <div className='col-7'>
-          <p className="card"><strong>Type:</strong> {movie.Type}</p>
-        </div>
-        <div className='col-8'>
-          <p className="card"><strong>Plot:</strong> {movie.Plot}</p>
-        </div>
+    <div className="movie-row-main">
+      <div className="col1">
+        <img src={movie.Poster} className="card-img-top" alt={movie.Title} />
+      </div>
+      <div className="col2">
+        <h2 className="card">Name: {movie.Title}</h2>
+        <p className="card">Actors: {movie.Actors}</p>
+        <p className="card">Released: {movie.Released}</p>
+        <p className="card">Director: {movie.Director}</p>
+        <p className="card">Genre: {movie.Genre}</p>
+        <p className="card">Country: {movie.Country}</p>
+        <p className="card">Type: {movie.Type}</p>
+        <p className="card">Plot: {movie.Plot}</p>
       </div>
     </div>
   );
 };
+
 
 export default MovieDetails;
