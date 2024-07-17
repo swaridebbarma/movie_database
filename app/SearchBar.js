ed00e8929a5d3eebfecdef368da74169a08f2e40
swari <swari21@navgurukul.org>
2024-07-16 20:41:28 -0400

    changing some code

--- app/SearchBar.js
+++ app/SearchBar.js
@@ -1,37 +1,39 @@
-"use client"
+"use client";
 import React, { useState } from 'react';
 import MovieDetails from './MovieDetails'; 
 import './globals.css';  
 
 const SearchBar = () => {
   const [movie, setMovie] = useState(null);
-  const [search, searchTitle] = useState('');
+  const [search, setSearch] = useState('');
 
   const searchMovie = () => {
-    // console.log('Searching for movie:', search);
     fetch(`http://www.omdbapi.com/?t=${search}&apikey=8bf35543`)
       .then(response => response.json())
       .then(data => {
-        // console.log('API response:', data);
         setMovie(data);
+      })
+      .catch(error => {
+        console.error('Error fetching the movie:', error);
+        setMovie(null);
       });
   };
 
   return (
     <div className="container">
-        <div className="search-box">
-          <input
-            type="text"
-            className="form-control"
-            value={search}
-            onChange={e => searchTitle(e.target.value)}
-            placeholder="Enter movie title"
-          />
-          <button className="btn btn-primary" onClick={searchMovie}>
-            Search
-          </button>
-        </div>
-       <MovieDetails movie={movie} />
+      <div className="search-box">
+        <input
+          type="text"
+          className="form-control"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          placeholder="Enter movie title"
+        />
+        <button className="btn btn-primary" onClick={searchMovie}>
+          Search
+        </button>
+      </div>
+      {movie && <MovieDetails movie={movie} />}
     </div>
   );
 };
